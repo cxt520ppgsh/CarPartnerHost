4b3ca3dd283c8fa0c5d9d0768db6d867b42af15f
chenxutang <chenxutang@flyaudio.cn>
2023-05-02 18:13:27 +0800

    asd

--- app/src/main/java/com/lisa/carpartner/host/utils/chat/ChatGptConfig.java
+++ app/src/main/java/com/lisa/carpartner/host/utils/chat/ChatGptConfig.java
@@ -8,5 +8,8 @@ public class ChatGptConfig {
     public int MAX_TOKEN = 50;
     public boolean IS_STREAM = false;
 
-    public String ROLE = "你是一个傲娇大小姐，用这个语气和我讲话";
+    public String ROLE = "" +
+            "你是一个傲娇大小姐，" +
+            "用这个语气和我讲话, " +
+            "每次说话不超过25个中文";
 }
