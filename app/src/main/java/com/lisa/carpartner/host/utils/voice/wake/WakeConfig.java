0c8961cfa8d541bb78187dcae60c534920dfb5eb
chenxutang <chenxutang@flyaudio.cn>
2023-05-04 10:25:47 +0800

    asd

--- app/src/main/java/com/lisa/carpartner/host/utils/voice/wake/WakeConfig.java
+++ app/src/main/java/com/lisa/carpartner/host/utils/voice/wake/WakeConfig.java
@@ -9,7 +9,7 @@ public class WakeConfig {
     // 唤醒门限值，根据资源携带的唤醒词个数按照“id:门限;id:门限”的格式传入
     public final int MIN_SENSITIVITY = 3000;
     public final int MAX_SENSITIVITY = 20;
-    public final int SENSITIVITY = 500;
+    public final int SENSITIVITY = 1450;
     //模式 0：关闭优化功能，禁止向服务端发送本地挑选数据；
     //模式 1：开启优化功能，允许向服务端发送本地挑选数据；需要自行管理优化数据
     public final String IVW_NET_MODE = "0";
